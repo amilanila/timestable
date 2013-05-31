f151639e85f10bd5bf522e06140d91b0378785be
amilanila@gmail.com <amilanila@gmail.com>
2013-05-31 17:04:26 +1000

    Reset answers feature

--- assets/www/js/game.js
+++ assets/www/js/game.js
@@ -1,24 +1,43 @@
 jQuery(document).ready(function($){
 //	app.initialize();
 	
+    init();
+
+    $('.next-question').click(function(){
+        if(questionIndex < 12){                        
+            init();            
+            question();                    
+        }        
+    });
+    
+    question();    
+});
+
+
+var questionIndex = 0;
+var multipliees = shuffle([1,2,3,4,5,6,7,8,9,10,11,12]);
+var answer = 0;
+
+function init(){
+    $('.draggable').remove();
+    $('.answer-box-container').append('<div class="draggable number-box number" onclick="void(0)"><span class="answer-given one"></span></div>');
+    $('.answer-box-container').append('<div class="draggable number-box number" onclick="void(0)"><span class="answer-given two"></span></div>');
+    $('.answer-box-container').append('<div class="draggable number-box number" onclick="void(0)"><span class="answer-given three"></span></div>');
+    $('.answer-box-container').append('<div class="draggable number-box number" onclick="void(0)"><span class="answer-given four"></span></div>');    
+
     $('#correct').hide();
     $('#incorrect').hide();
 
-	$('.draggable').draggable();
-	$('#droppybox').droppable({
-		drop: function(event, ui){
-			$(this).removeClass('vacant').addClass('occupied');
+    $('.draggable').draggable();
+    $('#droppybox').droppable({
+        drop: function(event, ui){
+            $(this).removeClass('vacant').addClass('occupied');
             $(ui.draggable).detach().css({top: 0,left: 0}).appendTo(this);
-            disableAnswering();
+            //disableAnswering();
             checkAnswer(this);
-		}
-	});    
-
-    question();
-});
-
-var multipliees = shuffle([1,2,3,4,5,6,7,8,9,10,11,12]);
-var answer = 0;
+        }
+    }); 
+}
 
 function checkAnswer(elem){
     var selectedAns = $(elem).children('.draggable ').children('.answer-given').text();
@@ -36,9 +55,14 @@ function disableAnswering(){
     $('.draggable').draggable('disable');    
 };
 
+function enableAnswering(){
+    $('.draggable').draggable('enable');    
+};
+
 function question(){
     var multiplier = getParameterByName('multiplier');
-    var multipliee = multipliees[0];
+    var multipliee = multipliees[questionIndex];
+    questionIndex = questionIndex + 1;
     answer = parseInt(multiplier) * parseInt(multipliee);
 
     askQuestion(multiplier, multipliee);
